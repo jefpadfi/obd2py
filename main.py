9f6460b2226e2d1dc1b7693ef3d9c87d349202d3
the_doctor <jr@apvesta.com>
2014-07-15 14:11:37 -0600

    First commit

---
+++ main.py
@@ -0,0 +1,6 @@
+'''
+Programmer: JR Padfield
+Description: Launches the program
+Version: 1
+Date: 07/15/2014
+'''
---
+++ obddata.py
@@ -0,0 +1,6 @@
+'''
+Programmer: JR Padfield
+Description: Pulls information from the obd 2 sensors.
+Version: 1
+Date: 07/15/2014
+'''
